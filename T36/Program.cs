6e4daf7476803fa5bf7f049019da8dc393751b74
Anton Stepin <ooosg@rambler.ru>
2022-05-26 11:35:22 +0300

    T36

---
+++ T36/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+int[] FillArray(int length, int deviation)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(-deviation, deviation + 1);
+    }
+    return result;
+}
+int SumOfOddPositionArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i = i + 2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите диапазон массива (массив будет от - до + вашего значения): ");
+int deviation = Convert.ToInt32(Console.ReadLine());
+if (deviation < 0) deviation = deviation * -1;
+int[] array = FillArray(length, deviation);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов нечетных позиций = {SumOfOddPositionArray(array)}");
